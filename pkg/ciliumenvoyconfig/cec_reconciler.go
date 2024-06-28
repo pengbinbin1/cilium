pengbinbin1/cilium

commit c3fcea21b6575148fe1ebb64fb9d65356607382a

gateway-api: Add periodic headless service sync
This is to handle the case of late arrival or late creation of Endpoint
or EndpointSlice associated to headless service.

Fixes: cce40804c3ac9f564859d788faef981a697de7ac
Signed-off-by: Tam Mach <tam.mach@cilium.io>

pkg/ciliumenvoyconfig/cec_reconciler.go
@@ -269,3 +269,20 @@ func (r *ciliumEnvoyConfigReconciler) configSelectsLocalNode(cfg *config) (bool,
 
 	return true, nil
 }
+
+func (r *ciliumEnvoyConfigReconciler) syncHeadlessService(_ context.Context) error {
+	r.mutex.Lock()
+	defer r.mutex.Unlock()
+
+	var reconcileErr error
+
+	for key, cfg := range r.configs {
+		if err := r.manager.syncCiliumEnvoyConfigService(cfg.meta.Name, cfg.meta.Namespace, cfg.spec); err != nil {
+			r.logger.WithField("key", key).WithError(err).Error("failed to sync headless service")
+			reconcileErr = errors.Join(reconcileErr, fmt.Errorf("failed to reconcile existing config (%s): %w", key, err))
+			continue
+		}
+	}
+
+	return reconcileErr
+}

pkg/ciliumenvoyconfig/cell.go
@@ -98,6 +98,7 @@ func registerCECK8sReconciler(params reconcilerParams) {
 	// This covers the cases were the reconciliation fails after changing the labels of a node.
 	if params.Config.EnvoyConfigRetryInterval > 0 {
 		params.JobGroup.Add(job.Timer("reconcile-existing-configs", reconciler.reconcileExistingConfigs, params.Config.EnvoyConfigRetryInterval))
+		params.JobGroup.Add(job.Timer("sync-headless-service", reconciler.syncHeadlessService, params.Config.EnvoyConfigRetryInterval))
 	}
 }
 

.github/workflows/conformance-gateway-api.yaml
@@ -138,6 +138,7 @@ jobs:
 
           CILIUM_INSTALL_DEFAULTS="${{ steps.default_vars.outputs.cilium_install_defaults }} \
             --helm-set=kubeProxyReplacement=true \
+            --helm-set=extraArgs={--envoy-config-retry-interval=5s} \
             --helm-set=gatewayAPI.enabled=true \
             --helm-set=l2announcements.enabled=true"
 

pkg/ciliumenvoyconfig/cec_manager.go
@@ -28,6 +28,7 @@ type ciliumEnvoyConfigManager interface {
 	addCiliumEnvoyConfig(cecObjectMeta metav1.ObjectMeta, cecSpec *ciliumv2.CiliumEnvoyConfigSpec) error
 	updateCiliumEnvoyConfig(oldCECObjectMeta metav1.ObjectMeta, oldCECSpec *ciliumv2.CiliumEnvoyConfigSpec, newCECObjectMeta metav1.ObjectMeta, newCECSpec *ciliumv2.CiliumEnvoyConfigSpec) error
 	deleteCiliumEnvoyConfig(cecObjectMeta metav1.ObjectMeta, cecSpec *ciliumv2.CiliumEnvoyConfigSpec) error
+	syncCiliumEnvoyConfigService(name string, namespace string, cecSpec *ciliumv2.CiliumEnvoyConfigSpec) error
 }
 
 type cecManager struct {
@@ -145,6 +146,12 @@ func (r *cecManager) addK8sServiceRedirects(resourceName service.L7LBResourceNam
 			return err
 		}
 	}
+	return r.syncCiliumEnvoyConfigService(resourceName.Name, resourceName.Namespace, spec)
+}
+
+func (r *cecManager) syncCiliumEnvoyConfigService(name string, namespace string, spec *ciliumv2.CiliumEnvoyConfigSpec) error {
+	resourceName := service.L7LBResourceName{Name: name, Namespace: namespace}
+
 	// Register services for Envoy backend sync
 	for _, svc := range spec.BackendServices {
 		serviceName := getServiceName(resourceName, svc.Name, svc.Namespace, false)
@@ -178,7 +185,6 @@ func (r *cecManager) addK8sServiceRedirects(resourceName service.L7LBResourceNam
 			}
 		}
 	}
-
 	return nil
 }
 

pkg/ciliumenvoyconfig/cec_reconciler_test.go
@@ -788,3 +788,7 @@ func (r *fakeCECManager) updateCiliumEnvoyConfig(oldCECObjectMeta metav1.ObjectM
 
 	return nil
 }
+
+func (r *fakeCECManager) syncCiliumEnvoyConfigService(name string, namespace string, cecSpec *ciliumv2.CiliumEnvoyConfigSpec) error {
+	return nil
+}

commit e949528dcd5c86aeff14bc216fa13c75af2be682

gateway-api: Add service observable event handler
This is to react based on events instead of relying on periodic sync.

Fixes: c3fcea21b6575148fe1ebb64fb9d65356607382a
Fixes: #33279
Signed-off-by: Tam Mach <tam.mach@cilium.io>

pkg/ciliumenvoyconfig/cec_reconciler.go
@@ -13,6 +13,7 @@ import (
 	"github.com/sirupsen/logrus"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
+	"github.com/cilium/cilium/pkg/k8s"
 	ciliumv2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
 	"github.com/cilium/cilium/pkg/k8s/resource"
 	"github.com/cilium/cilium/pkg/k8s/slim/k8s/apis/labels"
@@ -310,5 +311,29 @@ func (r *ciliumEnvoyConfigReconciler) syncHeadlessService(_ context.Context) err
 		}
 	}
 
+	return reconcileErr
+}
+
+func (r *ciliumEnvoyConfigReconciler) syncHeadlessEndpoints(_ context.Context, event resource.Event[*k8s.Endpoints]) error {
+	r.mutex.Lock()
+	defer r.mutex.Unlock()
+
+	var reconcileErr error
+	defer event.Done(reconcileErr)
+
+	for key, cfg := range r.configs {
+		for _, svc := range cfg.spec.BackendServices {
+			if svc.Name != event.Object.ServiceID.Name || svc.Namespace != event.Object.ServiceID.Namespace {
+				continue
+			}
+			err := r.manager.syncCiliumEnvoyConfigService(cfg.meta.Name, cfg.meta.Namespace, cfg.spec)
+			if err != nil {
+				r.logger.WithField("key", key).
+					WithField(logfields.ServiceKey, event.Key).
+					WithError(err).Error("failed to sync headless service")
+				reconcileErr = errors.Join(reconcileErr, fmt.Errorf("failed to sync headless service (%s): %w", key, err))
+			}
+		}
+	}
 	return reconcileErr
 }

pkg/ciliumenvoyconfig/cell.go
@@ -66,6 +66,8 @@ type reconcilerParams struct {
 	CECResources   resource.Resource[*ciliumv2.CiliumEnvoyConfig]
 	CCECResources  resource.Resource[*ciliumv2.CiliumClusterwideEnvoyConfig]
 	LocalNodeStore *node.LocalNodeStore
+
+	EndpointResources resource.Resource[*k8s.Endpoints]
 }
 
 func registerCECK8sReconciler(params reconcilerParams) {
@@ -113,6 +115,9 @@ func registerCECK8sReconciler(params reconcilerParams) {
 	// Observing local node events for changed labels
 	// Note: LocalNodeStore (in comparison to `resource.Resource`) doesn't provide a retry mechanism
 	params.JobGroup.Add(job.Observer("local-node-events", reconciler.handleLocalNodeEvent, params.LocalNodeStore))
+
+	// Observing service events for headless services
+	params.JobGroup.Add(job.Observer("headless-endpoint-events", reconciler.syncHeadlessEndpoints, params.EndpointResources))
 
 	// TimerJob periodically reconciles all existing configs.
 	// This covers the cases were the reconciliation fails after changing the labels of a node.